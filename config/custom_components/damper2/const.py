ee2a79f5c78a6e818eeff2c8c5a4614e8b634f15
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-09-14 19:42:13 +0000

    custom

---
+++ config/custom_components/damper2/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Coronavirus integration."""
+from coronavirus import DEFAULT_SOURCE
+
+DOMAIN = "damper2"
+OPTION_WORLDWIDE = "__worldwide"
+ATTRIBUTION = f"Data provided by {DEFAULT_SOURCE.NAME}"
