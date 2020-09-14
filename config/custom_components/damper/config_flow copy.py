a740b36b546f903753cc0544aca7ed19dacdb2db
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-09-14 18:07:13 +0000

    step 1

--- config/custom_components/damper/config_flow copy.py
+++ config/custom_components/damper/config_flow copy.py
@@ -1,14 +1,12 @@
-import logging
-
 import voluptuous as vol
 
 from homeassistant import config_entries
 
-# from . import get_coordinator
 from .const import DOMAIN
 
 # from homeassistant import data_entry_flow
-_LOGGER = logging.getLogger(__name__)
+
+
 
 # class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
@@ -24,15 +22,7 @@ _LOGGER = logging.getLogger(__name__)
 #         )
 
 # class ExampleConfigFlow(data_entry_flow.FlowHandler):
-
-
 class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-
-    _options = None
-
     async def async_step_user(self, user_input=None):
         # Specify items in the order they are to be displayed in the UI:mm
         data_schema = {
