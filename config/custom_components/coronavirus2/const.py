0aa775187e202299b5cf9c456b51d90934f2f1e7
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-09-14 19:30:08 +0000

    removed Config

--- config/custom_components/coronavirus2/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Coronavirus integration."""
-from coronavirus import DEFAULT_SOURCE
-
-DOMAIN = "coronavirus2"
-OPTION_WORLDWIDE = "__worldwide"
-ATTRIBUTION = f"Data provided by {DEFAULT_SOURCE.NAME}"
