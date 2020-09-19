58ceadac1b24f117d1f9794efa41ef91b205d3d9
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-09-19 09:18:11 +0000

    backup

--- config/custom_components/damper/config_flow.py
+++ config/custom_components/damper/config_flow.py
@@ -4,15 +4,13 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 
+# from homeassistant import data_entry_flow
 # from . import get_coordinator
 from .const import DOMAIN
 
-# from homeassistant import data_entry_flow
 _LOGGER = logging.getLogger(__name__)
 
 # class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
-
 #     VERSION = 1
 
 #     async def async_step_user(self, info):
@@ -26,32 +24,41 @@ _LOGGER = logging.getLogger(__name__)
 # class ExampleConfigFlow(data_entry_flow.FlowHandler):
 
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class DamperConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    _options = None
+    _availableAddresses = None
+    _availableAddresses = [1, 2, 3, 4]
 
     async def async_step_user(self, user_input=None):
-        # Specify items in the order they are to be displayed in the UI:mm
+        # Specify items in the order they are to be displayed in the UI
         errors = {}
 
         if user_input is not None:
             return self.async_abort
 
-        data_schema = {
-            vol.Required("username"): str,
-            vol.Required("password"): str,
-        }
-
-        # data_schema = {vol.Required("username"): str}
-
-        # if self.show_advanced_options:
-        #     data_schema["allow_groups"]: bool
+        data_schema = vol.Schema(
+            {vol.Required("nextAddress"): vol.In(self._availableAddresses)}
+        )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(data_schema),
+            data_schema=data_schema,
             errors=errors,
         )
+
+        # data_schema = {
+        #     vol.Required("username"): str,
+        #     vol.Required("password"): str,
+        # }
+
+        # return self.async_show_form(
+        #     step_id="user",
+        #     data_schema=vol.Schema(data_schema),
+        #     errors=errors,
+        # )
+
+        # if self.show_advanced_options:
+        #     data_schema["allow_groups"]: bool
