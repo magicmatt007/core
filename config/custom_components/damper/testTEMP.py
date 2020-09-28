3f87958f1d9744962f71f329f796ad5bd2959f8a
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-09-28 19:19:44 +0000

    backup

--- config/custom_components/damper/testTEMP.py
+++ config/custom_components/damper/testTEMP.py
@@ -1,12 +1,17 @@
-import time
-from datetime import date
+# import time
+# from datetime import date
 
-day = 31
-month = 12
-year = 2020
+# day = 31
+# month = 12
+# year = 2020
 
-dateobj = date(year, month, day)
-datestr = str(dateobj)
+# dateobj = date(year, month, day)
+# datestr = str(dateobj)
 
-print(type(dateobj))
-print(type(datestr))
+# print(type(dateobj))
+# print(type(datestr))
+
+asn = "GMA151.1E/IV"
+asn = ""
+asn_short = asn[0:6]
+print(asn_short)
