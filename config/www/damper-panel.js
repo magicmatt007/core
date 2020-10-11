7307c37e73b784afba4ad7bc47d54435f278886b
magicmatt007 <58187053+magicmatt007@users.noreply.github.com>
2020-10-11 20:08:56 +0000

    Code modernization

--- config/www/damper-panel.js
+++ config/www/damper-panel.js
@@ -7,9 +7,11 @@ import {
 
 import { classMap } from 'https://unpkg.com/lit-html/directives/class-map.js?module';
 
+// import "https://www.unpkg.com/@material/mwc-button@0.19.1/mwc-button.js?module";
+
 // import { LitElement, html, css } from 'lit-element';
 // import { classMap } from 'lit-html/directives/class-map';
-
+// unpkg.com/:package@:version/:file
 
 class DamperPanel extends LitElement {
   static get properties() {
@@ -25,60 +27,127 @@ class DamperPanel extends LitElement {
     super();
     // this.name = "World";
     // this.myArray = ["a", "b", "c"];
+    // this.i1 = this.hass.states[i].attributes["Last runtime open indicator"];
+    this.i1 = "WARNING";
+    this.showAttributes = ["Modbus Address", "friendly_name", "Type ASN", "current_position", "Last runtime open",
+      "Last runtime close", "Last power", "Last runtime open indicator", "Last runtime close indicator", "Last power indicator", "Last tested at"];
   }
 
   render() {
-    return html`
-      <wired-card elevation="2">
-      <table>
-      <tr>
-      <th>Modbus Address</th>
-      <th>Name</th>
-      <th>Model</th>
-      <th>State</th>
-      <th>Current position</th>
-      <th>Entity ID</th>
-      <th>Test now</th>
-      <th>Last runtime open</th>
-      <th>Last runtime close</th>
-      <th>Last power</th>
-      <th>Last runtime open indicator</th>
-      <th>Last runtime close indicator</th>
-      <th>Last power indicator</th>
-      <th>Last tested at</th>
-      </tr>
-      ${Object.keys(this.hass.states)
-        .filter((i) => i.startsWith("cover."))
-        .map(
-          (i) => html`
-          <tr>
-              <td>${this.hass.states[i].attributes["Modbus Address"]}</td>
-              <td>${this.hass.states[i].attributes["friendly_name"]}</td>
-              <td>${this.hass.states[i].attributes["Type ASN"]}</td>
-              <td>${this.hass.states[i].state}</td>
-              <td>${this.hass.states[i].attributes["current_position"]}</td>
-              <td>${this.hass.states[i]["entity_id"]}</td>
-              <td><button @click="${e => this.callServiceTest(this.hass.states[i]["entity_id"])}">Click me</button></td>
-              <td>${this.hass.states[i].attributes["Last runtime open"]}</td>
-              <td>${this.hass.states[i].attributes["Last runtime close"]}</td>
-              <td>${this.hass.states[i].attributes["Last power"]}</td>
-              <td class=${classMap({ failure: this.hass.states[i].attributes["Last runtime open indicator"] == 'FAILURE' })}>
-              ${this.hass.states[i].attributes["Last runtime open indicator"]}
-              </td>
-              <td class=${classMap({ failure: this.hass.states[i].attributes["Last runtime close indicator"] == 'FAILURE' })}>
-              ${this.hass.states[i].attributes[
-            "Last runtime close indicator"
-            ]}
-            </td>
-            <td class=${classMap({ failure: this.hass.states[i].attributes["Last power indicator"] == 'FAILURE' })}>${this.hass.states[i].attributes["Last power indicator"]}</td>
-            <td>${this.hass.states[i].attributes["Last tested at"]}</td>
-            </tr>
-            `
-        )}
+    // const row = attr => html`<td>: ${this.hass.states["cover.modbus_1"].attributes[attr]}</td>`;
+    const header = html`<tr>${this.showAttributes.map(attr => html`<th>${attr}</th>`)}
+                  <th>State</th>
+                  <th>Test</th>
+                  </tr>`;
+    const row = entity_id => html`
+                            <tr>${this.showAttributes.map(attr =>
+      html`<td class=${classMap({
+        failure: this.hass.states[entity_id].attributes[attr] == 'FAILURE',
+        warning: this.hass.states[entity_id].attributes[attr] == 'WARNING',
+        pass: this.hass.states[entity_id].attributes[attr] == 'PASS'
+      })}>${this.hass.states[entity_id].attributes[attr]}</td>`)}
+                            <td>${this.hass.states[entity_id].state}</td>
+                            <td><button @click="${e => this.callServiceTest(entity_id)}">Click me</button></td>
+                            </tr>
+                            `;
+    const rows = Object.keys(this.hass.states)
+      .filter((i) => i.startsWith("cover."))
+      .map(
+        (i) => row(i)
+      );
 
-            </table>
-            </wired - card >
-            `;
+    // <td class=${classMap({ failure: this.hass.states[i].attributes["Last runtime open indicator"] == 'FAILURE' })}>
+
+    this.coverEntityIds = Object.keys(this.hass.states).filter((i) => i.startsWith("cover.")).map((i) => i);
+    console.log(this.coverEntityIds);
+    this.x = this.hass.states[this.coverEntityIds[0]].attributes;
+    console.log(this.x);
+    console.log(this.x["Type ASN"]);
+
+    this.renderedView = '';
+
+    this.coverEntityIds.forEach(cover => {
+      this.a = this.hass.states[cover].attributes;
+      console.log(this.a);
+    });
+
+    this.coverEntityIds = Object.keys(this.hass.states).filter((i) => i.startsWith("cover.")).map((i) => {
+      console.log(i);
+      console.log(this.hass.states[i].attributes);
+    });
+
+    return html`
+    <wired-card elevation="2">
+    <table>
+    ${header}
+    ${rows}
+    </table>
+    </wired-card>
+
+    `;
+
+
+
+
+    // return html`
+    //   <wired-card elevation="2">
+    //   <table>
+    //   <tr>
+    //   <th>Modbus Address</th>
+    //   <th>Name</th>
+    //   <th>Model</th>
+    //   <th>State</th>
+    //   <th>Current position</th>
+    //   <th>Entity ID</th>
+    //   <th>Test now</th>
+    //   <th>Last runtime open</th>
+    //   <th>Last runtime close</th>
+    //   <th>Last power</th>
+    //   <th>Last runtime open indicator</th>
+    //   <th>Last runtime open indicator</th>
+    //   <th>Last runtime close indicator</th>
+    //   <th>Last power indicator</th>
+    //   <th>Last tested at</th>
+    //   </tr>
+    //   ${Object.keys(this.hass.states)
+    //     .filter((i) => i.startsWith("cover."))
+    //     .map(
+    //       (i) =>
+    //         html`
+    //       <tr>
+    //           <td>${this.hass.states[i].attributes["Modbus Address"]}</td>
+    //           <td>${this.hass.states[i].attributes["friendly_name"]}</td>
+    //           <td>${this.hass.states[i].attributes["Type ASN"]}</td>
+    //           <td>${this.hass.states[i].state}</td>
+    //           <td>${this.hass.states[i].attributes["current_position"]}</td>
+    //           <td>${this.hass.states[i]["entity_id"]}</td>
+    //           <td><button @click="${e => this.callServiceTest(this.hass.states[i]["entity_id"])}">Click me</button></td>
+    //           <td>${this.hass.states[i].attributes["Last runtime open"]}</td>
+    //           <td>${this.hass.states[i].attributes["Last runtime close"]}</td>
+    //           <td>${this.hass.states[i].attributes["Last power"]}</td>
+    //           <td class=${classMap({ failure: this.i1 == 'FAILURE' })}>
+    //           ${this.i1}
+    //           </td>
+    //           <td class=${classMap({ failure: this.hass.states[i].attributes["Last runtime open indicator"] == 'FAILURE' })}>
+    //           ${this.hass.states[i].attributes["Last runtime open indicator"]}
+    //           </td>
+    //           <td class=${classMap({ failure: this.hass.states[i].attributes["Last runtime close indicator"] == 'FAILURE' })}>
+    //           ${this.hass.states[i].attributes[
+    //           "Last runtime close indicator"
+    //           ]}
+    //         </td>
+    //         <td class=${classMap({ failure: this.hass.states[i].attributes["Last power indicator"] == 'FAILURE' })}>${this.hass.states[i].attributes["Last power indicator"]}</td>
+    //         <td>${this.hass.states[i].attributes["Last tested at"]}</td>
+    //         </tr>
+    //         `
+
+
+    //     )
+    //   }
+    //   </table>
+    //   </wired - card >
+    //   `;
+
   }
 
   callServiceTest(entity_id) {
@@ -110,15 +179,21 @@ class DamperPanel extends LitElement {
         border-collapse: collapse;
         padding: 15px
       }
+      th {
+        background-color: lightgrey;
+      }
 
       .pass {
         background-color: green;
+        color: white
       }
       .warning {
-        background-color: yellow;
+        background-color: orange;
+        color: black
       }
       .failure {
         background-color: red;
+        color: black
       }
 
       `;
